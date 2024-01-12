b0d55550fd026b38f885601d24fee68005e82957
unknown <rubikonspace@ukr.net>
2024-01-12 17:13:59 +0200

    34

---
+++ models/products/Products.js
@@ -0,0 +1,16 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: { type: String, default: "hahaha" },
+    price: { type: Number, min: 1, index: true },
+    sale: { type: Number, match: /[a-z]/ },
+    imgURL: { type: String },
+    buff: Buffer,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const ProductModel = model("product", productSchema);
+
+export default ProductModel;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import { app } from "./app.js";
-import { Schema, model } from "mongoose";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const { PORT, DB_HOST } = process.env;
 
-const productSchema = new Schema({
-  name: { type: String, default: "hahaha" },
-  price: { type: Number, min: 1, index: true },
-  sale: { type: Number, match: /[a-z]/ },
-  imgURL: { type: String },
-  buff: Buffer,
-});
-
-app.listen(PORT, () => {
-  console.log("start serv on 3005 port");
-});
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`start serv on ${PORT} port`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
