d97765bc4c53b3b60a464cfb9285398dab628e88
unknown <rubikonspace@ukr.net>
2024-01-12 17:06:20 +0200

    555

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 import { app } from "./app.js";
+import { Schema, model } from "mongoose";
+import "dotenv/config";
 
-app.listen(3001, () => {
-  console.log("start serv on 3001 port");
+const { PORT, DB_HOST } = process.env;
+
+const productSchema = new Schema({
+  name: { type: String, default: "hahaha" },
+  price: { type: Number, min: 1, index: true },
+  sale: { type: Number, match: /[a-z]/ },
+  imgURL: { type: String },
+  buff: Buffer,
+});
+
+app.listen(PORT, () => {
+  console.log("start serv on 3005 port");
 });
